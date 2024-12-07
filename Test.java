3ea4cd00cf6e36c4a5494ab507d4b7cb83ca100e
Dinesh  Asokan <adgcdinesh@gmail.com>
2024-12-07 21:23:05 +0530

    Create Test.java
    java sample

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+        public static void main(String[] args) {
+               System.out.println("***********************************");
+               System.out.println("Hello from GITHUB");
+               System.out.println("************************************************************");
+        }
+}
