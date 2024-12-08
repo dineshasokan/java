f4ad099640571e109ee04e231077dd4d5bc3d535
Dinesh  Asokan <adgcdinesh@gmail.com>
2024-12-08 13:25:09 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
 
         public static void main(String[] args) {
                System.out.println("***********************************");
-               System.out.println("Hello from GITHUB");
+               System.out.println("Hello from GITHUB new");
                System.out.println("************************************************************");
         }
 }
